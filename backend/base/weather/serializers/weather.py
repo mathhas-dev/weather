4a6b7a7ddec088bedf6f610b17a5b76359e91ce7
Matheus Henrique <mathkeka@gmail.com>
2022-05-15 22:44:46 -0300

    Improving way to return forecasts

--- backend/base/weather/serializers/weather.py
+++ backend/base/weather/serializers/weather.py
@@ -1,10 +1,12 @@
-from weather.models import Weather
+from weather.models import Forecast, Weather
 from rest_framework import serializers
+from django.forms.models import model_to_dict
 import json
 
 
 class WeatherSerializer(serializers.ModelSerializer):
     results = serializers.SerializerMethodField()
+    forecast = serializers.SerializerMethodField()
 
     class Meta:
         model = Weather
@@ -14,3 +16,13 @@ class WeatherSerializer(serializers.ModelSerializer):
         results_json = json.loads(obj.results.replace("'", '"'))
 
         return results_json
+
+    def get_forecast(self, obj):
+        forecasts = Forecast.objects.filter(weather=obj).order_by('date_create')[:9]
+
+        forecast = []
+
+        for item in forecasts:
+            forecast.append(model_to_dict(item))
+
+        return forecast
